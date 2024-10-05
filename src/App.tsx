855432800f110c79d35ec0fda91d15751f1c684b
Thomas_465 <160943727+Thomas465xd@users.noreply.github.com>
2024-10-05 10:33:21 -0300

    *Base Structure for the project Defined -Added the background image and CSS modules

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styles from "./App.module.css"
+
+function App() {
+
+    return (
+        <>
+            <h1 className={styles.title}>Weather Searcher</h1>
+
+            <div className={styles.container}>
+                <p>1</p>
+                <p>2</p>
+            </div>
+        </>
+    )
+}
+
+export default App
